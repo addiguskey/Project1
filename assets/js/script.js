ccfaf3082a78148b4e2e10fe2795854326f29746
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-05-17 14:01:40 -0600

    update on random song gen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 // artist-top-20-tracks API Code
-const settings = {
-  async: true,
-  crossDomain: true,
-  url: "https://shazam.p.rapidapi.com/songs/ip-country-chart-US?id=5128581&locale=en-US",
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Host": "shazam.p.rapidapi.com",
-    "X-RapidAPI-Key": "62fd737dc8mshdc4b2103750a6cap1678d6jsn9a8771e39bcf",
-  },
-};
+// const settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: "https://shazam.p.rapidapi.com/songs/ip-country-chart-US?id=5128581&locale=en-US",
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+//     "X-RapidAPI-Key": "62fd737dc8mshdc4b2103750a6cap1678d6jsn9a8771e39bcf",
+//   },
+// };
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
 
 // random drinks API
 
@@ -23,3 +23,35 @@ $.ajax(settings).done(function (response) {
 // function for randomCocktail
 // function for randomMusic
 // Local Storage - like/dislike btn - store if like
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: `https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=10&startFrom=${getRandomInt(
+    190
+  )}`,
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+    "X-RapidAPI-Key": "62fd737dc8mshdc4b2103750a6cap1678d6jsn9a8771e39bcf",
+  },
+};
+
+$.ajax(settings).done(function (data) {
+  console.log(data);
+  for (let i = 0; i < data.tracks.length; i++) {
+    document.querySelector("body").innerHTML += data.tracks[i].title;
+  }
+});
+function getRandomInt(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+// fetch(
+//   `https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=10&startFrom=${getRandomInt(
+//     190
+//   )}`
+// )
+//   .then((res) => res.json())
+//   .then((data) => {
+//     document.querySelector("body").innerHTML = data.results[0].tracks;
+//   });
