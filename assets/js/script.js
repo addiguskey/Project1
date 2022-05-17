f983795dceb16bf859ec12eb7de7f458a4c6aae3
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-05-17 14:30:26 -0600

    for loop for song gen is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,3 @@
-// artist-top-20-tracks API Code
-// const settings = {
-//   async: true,
-//   crossDomain: true,
-//   url: "https://shazam.p.rapidapi.com/songs/ip-country-chart-US?id=5128581&locale=en-US",
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Host": "shazam.p.rapidapi.com",
-//     "X-RapidAPI-Key": "62fd737dc8mshdc4b2103750a6cap1678d6jsn9a8771e39bcf",
-//   },
-// };
-
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
-
 // random drinks API
 
 // set Variables
@@ -23,6 +7,8 @@
 // function for randomCocktail
 // function for randomMusic
 // Local Storage - like/dislike btn - store if like
+
+// 10 random songs from top charts API
 const settings = {
   async: true,
   crossDomain: true,
@@ -40,18 +26,24 @@ $.ajax(settings).done(function (data) {
   console.log(data);
   for (let i = 0; i < data.tracks.length; i++) {
     document.querySelector("body").innerHTML += data.tracks[i].title;
+    document.querySelector("body").innerHTML += data.tracks[i].subtitle;
   }
 });
 function getRandomInt(max) {
   return Math.ceil(Math.random() * max);
 }
 
-// fetch(
-//   `https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=10&startFrom=${getRandomInt(
-//     190
-//   )}`
-// )
-//   .then((res) => res.json())
-//   .then((data) => {
-//     document.querySelector("body").innerHTML = data.results[0].tracks;
-//   });
+const cocktailApi = {
+  async: true,
+  crossDomain: true,
+  url: "https://the-cocktail-db.p.rapidapi.com/random.php",
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
+    "X-RapidAPI-Key": "62fd737dc8mshdc4b2103750a6cap1678d6jsn9a8771e39bcf",
+  },
+};
+
+$.ajax(cocktailApi).done(function (response) {
+  console.log(response);
+});
