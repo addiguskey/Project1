bebbf1d181ba2f6e72d419f2b11db07371bccf13
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-05-18 14:58:12 -0600

    drinkimages working

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,129 +9,106 @@
 // Local Storage - like/dislike btn - store if like
 
 // 10 random songs from top charts API
-const settings = {
-  async: true,
-  crossDomain: true,
-  url: `https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=10&startFrom=${getRandomInt(
-    190
-  )}`,
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Host": "shazam.p.rapidapi.com",
-    "X-RapidAPI-Key": "146b5a78ebmsh9ba3fd635d9cbe4p17de03jsnfeb09829ed42",
-  },
-};
+// const settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: `https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=10&startFrom=${getRandomInt(
+//     190
+//   )}`,
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+//     "X-RapidAPI-Key": "146b5a78ebmsh9ba3fd635d9cbe4p17de03jsnfeb09829ed42",
+//   },
+// };
 
-$.ajax(settings).done(function (data) {
-  console.log(data, "musicData");
-  for (let i = 0; i < data.tracks.length; i++) {
-    // document.querySelector("body").innerHTML += data.tracks[i].title;
-    // document.querySelector("body").innerHTML += data.tracks[i].subtitle;
-  }
-});
-function getRandomInt(max) {
-  return Math.ceil(Math.random() * max);
-}
+// $.ajax(settings).done(function (data) {
+//   console.log(data, "musicData");
+//   for (let i = 0; i < data.tracks.length; i++) {
+//     // document.querySelector("body").innerHTML += data.tracks[i].title;
+//     // document.querySelector("body").innerHTML += data.tracks[i].subtitle;
+//   }
+// });
+// function getRandomInt(max) {
+//   return Math.ceil(Math.random() * max);
+// }
 
-
-const cocktailApi = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
-
-
-
-
+const cocktailApi = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+var drinkData;
 $.ajax({
   url: cocktailApi,
-  method: 'GET',
+  method: "GET",
   data: {
-    'api-key': '1'
+    "api-key": "1",
   },
 }).then(function (response) {
-  const drinkData = response.drinks[0]
+  drinkData = response.drinks[0];
 
   for (const key in drinkData) {
     if (drinkData[key] === null) {
-      delete drinkData[key]  
+      delete drinkData[key];
     }
   }
 
-
-  // document.querySelector("body").innerHTML += drinkData.strDrink
-  // document.querySelector("body").innerHTML += drinkData.strIngredient1
-  // document.querySelector("body").innerHTML += drinkData.strIngredient2
-  // document.querySelector("body").innerHTML += drinkData.strIngredient3
-  // document.querySelector("body").innerHTML += drinkData.strIngredient4
-  // document.querySelector("body").innerHTML += drinkData.strIngredient5
-  // document.querySelector("body").innerHTML += drinkData.strIngredient6
-  // document.querySelector("body").innerHTML += drinkData.strIngredient7
-  // document.querySelector("body").innerHTML += drinkData.strIngredient8
-  // document.querySelector("body").innerHTML += drinkData.strIngredient9
-  // document.querySelector("body").innerHTML += drinkData.strIngredient10
-  // document.querySelector("body").innerHTML += drinkData.strIngredient11
-  // document.querySelector("body").innerHTML += drinkData.strIngredient12
-  // document.querySelector("body").innerHTML += drinkData.strIngredient13
-  // document.querySelector("body").innerHTML += drinkData.strIngredient14
-  // document.querySelector("body").innerHTML += drinkData.strIngredient15
-  // document.querySelector("body").innerHTML += drinkData.strInstructions
-  
-  
-
-  for (const key in drinkData) {
-    if (drinkData[key] === null) {
-      delete drinkData[key]
-    }  
-  }
-  
-  
-  console.log(response)
+  console.log(response);
   console.log(drinkData.strDrink);
-  // console.log(drinkData.strIngredient1);
-  // console.log(drinkData.strIngredient2);
-  // console.log(drinkData.strIngredient3);
-  // console.log(drinkData.strIngredient4);
-  // console.log(drinkData.strIngredient5);
-  // console.log(drinkData.strIngredient6);
-  // console.log(drinkData.strIngredient7);
-  // console.log(drinkData.strIngredient8);
-  // console.log(drinkData.strIngredient9);
-  // console.log(drinkData.strIngredient10);
-  // console.log(drinkData.strIngredient11);
-  // console.log(drinkData.strIngredient12);
-  // console.log(drinkData.strIngredient13);
-  // console.log(drinkData.strIngredient14);
-  // console.log(drinkData.strIngredient15);
-  // console.log(drinkData.strstrInstructions);
-  var drinkEntriesDataArray = Object.entries(drinkData)
-  console.log("drinkEntriesDataArray",drinkEntriesDataArray);
-  var ingredientsArray = drinkEntriesDataArray.filter(([key, value]) => key.startsWith("strIngredient") && value && value.trim()).map(([key, value]) => value)
-  console.log("ingredientsArray",ingredientsArray);
-  var measuresArray = drinkEntriesDataArray.filter(([key, value]) => key.startsWith("strMeasure") && value && value.trim()).map(([key, value]) => value);
-  console.log("measuresArray",measuresArray);
+
+  var drinkEntriesDataArray = Object.entries(drinkData);
+  console.log("drinkEntriesDataArray", drinkEntriesDataArray);
+
+  var ingredientsArray = drinkEntriesDataArray
+    .filter(
+      ([key, value]) => key.startsWith("strIngredient") && value && value.trim()
+    )
+    .map(([key, value]) => value);
+  console.log("ingredientsArray", ingredientsArray);
+
+  var measuresArray = drinkEntriesDataArray
+    .filter(
+      ([key, value]) => key.startsWith("strMeasure") && value && value.trim()
+    )
+    .map(([key, value]) => value);
+  console.log("measuresArray", measuresArray);
+
+  console.log("imageArray", imageArray);
+  var imageArray = drinkEntriesDataArray
+    .filter(([key, value]) => key.startsWith("strDrinkThumb"))
+    .map(([key, value]) => value);
+  // .filter(
+  //   ([key, value]) =>
+  //     key.startsWith("strIngredient") && value && value.trim()
+  // )
+  // .map(([key, value]) => value);
 });
 
 //star play
-
-var drinckIconArray =[
+var drinckIconArray = [
   "./assets/images/drink-icon2.svg",
   "./assets/images/drink-icon3.svg",
   "./assets/images/drink-icon3.svg",
   "./assets/images/drink-icon5.svg",
   "./assets/images/drink-icon1.svg",
-]
+];
 var curImageIndex = 0; // 0 is displayed by default
 var intervalId; //Remember the ID of the interval so we can stop it later.
 
-$("#start").click(function startImageCycle(el){
-    cycleImage(el); //Cycle the image now so feels responsive. Remove if not wanted.
-    intervalId = setInterval(cycleImage,300,el); //Change image every 1000ms (1s)
-    $(".music-icon").addClass("spin");
-})
-function stopImageCycle(el){
-    clearInterval(intervalId);
+$("#start").click(function startImageCycle(el) {
+  cycleImage(el); //Cycle the image now so feels responsive. Remove if not wanted.
+  intervalId = setInterval(cycleImage, 300, el); //Change image every 1000ms (1s)
+  $(".music-icon").addClass("spin");
+  setTimeout(function () {
+    stopImageCycle();
+    $(".drink-icon").attr("src", drinkData.strDrinkThumb);
+    $(".drink-title").text(drinkData.strDrink);
+  }, 3000);
+});
+function stopImageCycle(el) {
+  clearInterval(intervalId);
 }
-function cycleImage(element){
-    curImageIndex++;
-    if(curImageIndex >= drinckIconArray.length) {
-        curImageIndex = 0;
-    }
-    $(".drink-icon").attr("src", drinckIconArray[curImageIndex]);
+function cycleImage(element) {
+  curImageIndex++;
+  if (curImageIndex >= drinckIconArray.length) {
+    curImageIndex = 0;
+  }
+  $(".drink-icon").attr("src", drinckIconArray[curImageIndex]);
 }
