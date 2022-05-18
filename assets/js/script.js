8711d5e928a8bf65dd06c2aff4547c97b2dd3a24
PeiYun <ollylee8520@gmail.com>
2022-05-18 13:07:06 -0600

    updated main page/css/js

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,23 +55,23 @@ $.ajax({
   }
 
 
-  document.querySelector("body").innerHTML += drinkData.strDrink
-  document.querySelector("body").innerHTML += drinkData.strIngredient1
-  document.querySelector("body").innerHTML += drinkData.strIngredient2
-  document.querySelector("body").innerHTML += drinkData.strIngredient3
-  document.querySelector("body").innerHTML += drinkData.strIngredient4
-  document.querySelector("body").innerHTML += drinkData.strIngredient5
-  document.querySelector("body").innerHTML += drinkData.strIngredient6
-  document.querySelector("body").innerHTML += drinkData.strIngredient7
-  document.querySelector("body").innerHTML += drinkData.strIngredient8
-  document.querySelector("body").innerHTML += drinkData.strIngredient9
-  document.querySelector("body").innerHTML += drinkData.strIngredient10
-  document.querySelector("body").innerHTML += drinkData.strIngredient11
-  document.querySelector("body").innerHTML += drinkData.strIngredient12
-  document.querySelector("body").innerHTML += drinkData.strIngredient13
-  document.querySelector("body").innerHTML += drinkData.strIngredient14
-  document.querySelector("body").innerHTML += drinkData.strIngredient15
-  document.querySelector("body").innerHTML += drinkData.strInstructions
+  // document.querySelector("body").innerHTML += drinkData.strDrink
+  // document.querySelector("body").innerHTML += drinkData.strIngredient1
+  // document.querySelector("body").innerHTML += drinkData.strIngredient2
+  // document.querySelector("body").innerHTML += drinkData.strIngredient3
+  // document.querySelector("body").innerHTML += drinkData.strIngredient4
+  // document.querySelector("body").innerHTML += drinkData.strIngredient5
+  // document.querySelector("body").innerHTML += drinkData.strIngredient6
+  // document.querySelector("body").innerHTML += drinkData.strIngredient7
+  // document.querySelector("body").innerHTML += drinkData.strIngredient8
+  // document.querySelector("body").innerHTML += drinkData.strIngredient9
+  // document.querySelector("body").innerHTML += drinkData.strIngredient10
+  // document.querySelector("body").innerHTML += drinkData.strIngredient11
+  // document.querySelector("body").innerHTML += drinkData.strIngredient12
+  // document.querySelector("body").innerHTML += drinkData.strIngredient13
+  // document.querySelector("body").innerHTML += drinkData.strIngredient14
+  // document.querySelector("body").innerHTML += drinkData.strIngredient15
+  // document.querySelector("body").innerHTML += drinkData.strInstructions
   
   
 
@@ -102,3 +102,31 @@ $.ajax({
   console.log(drinkData.strstrInstructions);
   
 });
+
+//star play
+
+var drinckIconArray =[
+  "./assets/images/drink-icon2.svg",
+  "./assets/images/drink-icon3.svg",
+  "./assets/images/drink-icon3.svg",
+  "./assets/images/drink-icon5.svg",
+  "./assets/images/drink-icon1.svg",
+]
+var curImageIndex = 0; // 0 is displayed by default
+var intervalId; //Remember the ID of the interval so we can stop it later.
+
+$("#start").click(function startImageCycle(el){
+    cycleImage(el); //Cycle the image now so feels responsive. Remove if not wanted.
+    intervalId = setInterval(cycleImage,300,el); //Change image every 1000ms (1s)
+    $(".music-icon").addClass("spin");
+})
+function stopImageCycle(el){
+    clearInterval(intervalId);
+}
+function cycleImage(element){
+    curImageIndex++;
+    if(curImageIndex >= drinckIconArray.length) {
+        curImageIndex = 0;
+    }
+    $(".drink-icon").attr("src", drinckIconArray[curImageIndex]);
+}
